e3fffc509da3d7779a28c2a28e9feb54ced82c23
Dengtiel <d.akol@alustudent.com>
2026-06-29 17:17:01 +0200

    strace task 2: print syscall name and return value in hex

--- strace/tracer.c
+++ strace/tracer.c
@@ -17,22 +17,21 @@ static void child_exec(char **argv)
 }
 
 /**
- * print_syscall_num - print syscall number from registers
- * @regs: pointer to user_regs_struct filled by ptrace
+ * print_num_mode - print syscall number mode
+ * @regs: registers from ptrace
  */
-static void print_syscall_num(struct user_regs_struct *regs)
+static void print_num_mode(struct user_regs_struct *regs)
 {
 	printf("%lu\n", (unsigned long)regs->orig_rax);
 	fflush(stdout);
 }
 
 /**
- * print_syscall_name_regs - print syscall name from registers
- * @regs: pointer to user_regs_struct filled by ptrace
- * @in_syscall: 0 = entry, 1 = exit
+ * print_name_mode - print syscall name mode
+ * @regs: registers from ptrace
+ * @in_syscall: 0 entry, 1 exit
  */
-static void print_syscall_name_regs(struct user_regs_struct *regs,
-	int in_syscall)
+static void print_name_mode(struct user_regs_struct *regs, int in_syscall)
 {
 	long num;
 	const char *name;
@@ -64,19 +63,63 @@ static void print_syscall_name_regs(struct user_regs_struct *regs,
 }
 
 /**
+ * print_retval_mode - print syscall name and return value
+ * @regs: registers from ptrace
+ * @in_syscall: 0 entry, 1 exit
+ * @last_syscall: syscall number saved at entry
+ */
+static void print_retval_mode(struct user_regs_struct *regs,
+	int in_syscall, long last_syscall)
+{
+	const char *name;
+	long retval;
+
+	name = get_syscall_name(last_syscall);
+	retval = (long)regs->rax;
+
+	if (last_syscall == 1)
+	{
+		if (in_syscall == 0)
+		{
+			printf("%s", name ? name : "unknown");
+			fflush(stdout);
+		}
+		else
+		{
+			printf(" = 0x%lx\n", retval);
+			fflush(stdout);
+		}
+		return;
+	}
+	if (in_syscall == 1)
+	{
+		if (name != NULL)
+			printf("%s = ", name);
+		else
+			printf("unknown_%ld = ", last_syscall);
+		if (name && strcmp(name, "exit_group") == 0)
+			printf("?\n");
+		else
+			printf("0x%lx\n", retval);
+		fflush(stdout);
+	}
+}
+
+/**
  * run_strace - fork, exec under ptrace, intercept syscalls
  * @argc: argument count
  * @argv: argument vector; argv[1] is the traced program
- * @print_name: 0 for number, 1 for name
+ * @mode: 0=number, 1=name, 2=name+retval
  *
  * Return: exit status of traced process, or 1 on error
  */
-int run_strace(int argc, char **argv, int print_name)
+int run_strace(int argc, char **argv, int mode)
 {
 	pid_t child;
 	int status;
 	struct user_regs_struct regs;
 	int in_syscall;
+	long last_syscall;
 
 	if (argc < 2)
 	{
@@ -95,13 +138,16 @@ int run_strace(int argc, char **argv, int print_name)
 	waitpid(child, &status, 0);
 	ptrace(PTRACE_SETOPTIONS, child, 0, PTRACE_O_TRACESYSGOOD);
 
-	if (print_name)
+	if (mode == 1)
 		printf("execve\n");
-	else
+	else if (mode == 0)
 		printf("59\n");
+	else
+		printf("execve = 0\n");
 	fflush(stdout);
 
 	in_syscall = 0;
+	last_syscall = -1;
 	while (1)
 	{
 		ptrace(PTRACE_SYSCALL, child, NULL, NULL);
@@ -118,12 +164,23 @@ int run_strace(int argc, char **argv, int print_name)
 
 		ptrace(PTRACE_GETREGS, child, NULL, &regs);
 
-		if (print_name)
-			print_syscall_name_regs(&regs, in_syscall);
-		else if (in_syscall == 0)
-			print_syscall_num(&regs);
+		if (in_syscall == 0)
+			last_syscall = (long)regs.orig_rax;
+
+		if (mode == 0 && in_syscall == 0)
+			print_num_mode(&regs);
+		else if (mode == 1)
+			print_name_mode(&regs, in_syscall);
+		else if (mode == 2)
+			print_retval_mode(&regs, in_syscall, last_syscall);
 
 		in_syscall ^= 1;
+	}
+	/* print exit_group = ? */
+	if (mode == 2)
+	{
+		printf("exit_group = ?\n");
+		fflush(stdout);
 	}
 	return (WIFEXITED(status) ? WEXITSTATUS(status) : 1);
 }
