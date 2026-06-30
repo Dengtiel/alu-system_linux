da5da78df8b810fcb56a5c61705868921705a9f6
Dengtiel <d.akol@alustudent.com>
2026-06-30 22:15:09 +0200

    Fix: no symbols stderr, Betty 40-line limit, INIT/FINI_ARRAY type

--- nm_objdump/elf32.c
+++ nm_objdump/elf32.c
@@ -1,42 +1,54 @@
 #include "hnm.h"
 
 /**
- * read_sym_table32 - find and read symbol table from 32-bit ELF
- * @fd: file descriptor (seeked to start)
- * @ehdr: pointer to ELF header
+ * find_symtab32 - find symbol table and string table indices
  * @shdrs: array of section headers
- * @symcount: output - number of symbols found
- * @strtab_off: output - file offset of string table
+ * @shnum: number of sections
+ * @sym_idx: output symbol table index
+ * @str_idx: output string table index
  * @swap: non-zero if byte-swapping needed
- *
- * Return: allocated array of Elf32_Sym, or NULL on failure
  */
-static Elf32_Sym *read_sym_table32(int fd, Elf32_Ehdr *ehdr,
-		Elf32_Shdr *shdrs, size_t *symcount,
-		uint32_t *strtab_off, int swap)
+static void find_symtab32(Elf32_Shdr *shdrs, int shnum,
+		int *sym_idx, int *str_idx, int swap)
 {
-	int i, shnum, sym_idx = -1, str_idx = -1;
-	Elf32_Shdr *sh;
-	Elf32_Sym *syms;
-	uint32_t offset, size, link;
-	ssize_t n;
+	int i;
+	uint32_t type, link;
 
-	shnum = swap ? (int)swap16(ehdr->e_shnum) : (int)ehdr->e_shnum;
+	*sym_idx = -1;
+	*str_idx = -1;
 	for (i = 0; i < shnum; i++)
 	{
-		sh = &shdrs[i];
-		uint32_t type = swap ? swap32(sh->sh_type) : sh->sh_type;
-
+		type = swap ? swap32(shdrs[i].sh_type) : shdrs[i].sh_type;
 		if (type == SHT_SYMTAB)
 		{
-			sym_idx = i;
-			link = swap ? swap32(sh->sh_link) : sh->sh_link;
-			str_idx = (int)link;
+			*sym_idx = i;
+			link = swap ? swap32(shdrs[i].sh_link)
+				: shdrs[i].sh_link;
+			*str_idx = (int)link;
 			break;
 		}
 	}
-	if (sym_idx < 0 || str_idx < 0 || str_idx >= shnum)
-		return (NULL);
+}
+
+/**
+ * read_sym_table32 - read symbol table from 32-bit ELF
+ * @fd: file descriptor
+ * @shdrs: array of section headers
+ * @sym_idx: symbol table section index
+ * @str_idx: string table section index
+ * @symcount: output number of symbols
+ * @strtab_off: output string table file offset
+ * @swap: non-zero if byte-swapping needed
+ *
+ * Return: allocated Elf32_Sym array or NULL
+ */
+static Elf32_Sym *read_sym_table32(int fd, Elf32_Shdr *shdrs,
+		int sym_idx, int str_idx, size_t *symcount,
+		uint32_t *strtab_off, int swap)
+{
+	uint32_t offset, size;
+	Elf32_Sym *syms;
+	ssize_t n;
 
 	offset = swap ? swap32(shdrs[sym_idx].sh_offset)
 		: shdrs[sym_idx].sh_offset;
@@ -45,7 +57,6 @@ static Elf32_Sym *read_sym_table32(int fd, Elf32_Ehdr *ehdr,
 	*strtab_off = swap ? swap32(shdrs[str_idx].sh_offset)
 		: shdrs[str_idx].sh_offset;
 	*symcount = size / sizeof(Elf32_Sym);
-
 	syms = malloc(size);
 	if (!syms)
 		return (NULL);
@@ -60,60 +71,46 @@ static Elf32_Sym *read_sym_table32(int fd, Elf32_Ehdr *ehdr,
 		free(syms);
 		return (NULL);
 	}
-	(void)ehdr;
 	return (syms);
 }
 
 /**
- * build_entries32 - build sym_entry_t array from 32-bit symbol table
+ * build_entry32 - build one sym_entry_t from a 32-bit symbol
  * @fd: file descriptor
- * @syms: array of Elf32_Sym
+ * @sym: pointer to Elf32_Sym
  * @shdrs: array of section headers
- * @symcount: number of symbols
  * @strtab_off: string table file offset
  * @shnum: number of sections
  * @swap: non-zero if byte-swapping needed
  *
- * Return: allocated array of sym_entry_t or NULL on failure
+ * Return: populated sym_entry_t
  */
-static sym_entry_t *build_entries32(int fd, Elf32_Sym *syms,
-		Elf32_Shdr *shdrs, size_t symcount,
-		uint32_t strtab_off, int shnum, int swap)
+static sym_entry_t build_entry32(int fd, Elf32_Sym *sym,
+		Elf32_Shdr *shdrs, uint32_t strtab_off,
+		int shnum, int swap)
 {
-	sym_entry_t *entries;
-	size_t i;
-	uint32_t name_off;
+	sym_entry_t entry;
+	uint32_t name_off, val;
 	char namebuf[256];
 	ssize_t n;
-	uint32_t val;
 
-	entries = malloc(symcount * sizeof(sym_entry_t));
-	if (!entries)
-		return (NULL);
-
-	for (i = 0; i < symcount; i++)
+	name_off = swap ? swap32(sym->st_name) : sym->st_name;
+	if (lseek(fd, strtab_off + name_off, SEEK_SET) < 0)
 	{
-		name_off = swap ? swap32(syms[i].st_name) : syms[i].st_name;
-		if (lseek(fd, strtab_off + name_off, SEEK_SET) < 0)
-		{
-			entries[i].name = strdup("");
-		}
-		else
-		{
-			n = read(fd, namebuf, sizeof(namebuf) - 1);
-			if (n <= 0)
-				namebuf[0] = '\0';
-			else
-				namebuf[n] = '\0';
-			namebuf[sizeof(namebuf) - 1] = '\0';
-			entries[i].name = strdup(namebuf);
-		}
-		val = swap ? swap32(syms[i].st_value) : syms[i].st_value;
-		entries[i].value = (unsigned long)val;
-		entries[i].type = get_sym_type32(&syms[i], shdrs, shnum, swap);
-		entries[i].has_value = (syms[i].st_shndx != SHN_UNDEF);
+		entry.name = strdup("");
 	}
-	return (entries);
+	else
+	{
+		n = read(fd, namebuf, sizeof(namebuf) - 1);
+		namebuf[n > 0 ? n : 0] = '\0';
+		namebuf[sizeof(namebuf) - 1] = '\0';
+		entry.name = strdup(namebuf);
+	}
+	val = swap ? swap32(sym->st_value) : sym->st_value;
+	entry.value = (unsigned long)val;
+	entry.type = get_sym_type32(sym, shdrs, shnum, swap);
+	entry.has_value = (sym->st_shndx != SHN_UNDEF);
+	return (entry);
 }
 
 /**
@@ -130,41 +127,41 @@ static int read_elf32_core(int fd, const char *filename, int swap)
 	Elf32_Shdr *shdrs = NULL;
 	Elf32_Sym *syms = NULL;
 	sym_entry_t *entries = NULL;
-	size_t symcount = 0;
+	size_t symcount = 0, i;
 	uint32_t strtab_off = 0;
-	int shnum, ret = 1;
+	int shnum, sym_idx, str_idx, ret = 1;
 	ssize_t n;
-	uint32_t shoff;
-	size_t shsize;
 
 	n = read(fd, &ehdr, sizeof(Elf32_Ehdr));
 	if (n < (ssize_t)sizeof(Elf32_Ehdr))
-	{
-		fprintf(stderr, "hnm: %s: unexpected end of file\n", filename);
 		return (1);
-	}
 	shnum = swap ? (int)swap16(ehdr.e_shnum) : (int)ehdr.e_shnum;
-	shoff = swap ? swap32(ehdr.e_shoff) : ehdr.e_shoff;
-	shsize = (size_t)shnum * sizeof(Elf32_Shdr);
-	shdrs = malloc(shsize);
+	shdrs = malloc((size_t)shnum * sizeof(Elf32_Shdr));
 	if (!shdrs)
 		return (1);
-	if (lseek(fd, shoff, SEEK_SET) < 0)
+	if (lseek(fd, swap ? swap32(ehdr.e_shoff) : ehdr.e_shoff,
+		SEEK_SET) < 0)
 		goto cleanup;
-	n = read(fd, shdrs, shsize);
-	if (n < 0 || (size_t)n < shsize)
+	n = read(fd, shdrs, (size_t)shnum * sizeof(Elf32_Shdr));
+	if (n < 0 || (size_t)n < (size_t)shnum * sizeof(Elf32_Shdr))
 		goto cleanup;
-	syms = read_sym_table32(fd, &ehdr, shdrs, &symcount,
-				&strtab_off, swap);
-	if (!syms)
+	find_symtab32(shdrs, shnum, &sym_idx, &str_idx, swap);
+	if (sym_idx < 0 || str_idx < 0 || str_idx >= shnum)
 	{
+		fprintf(stderr, "hnm: %s: no symbols\n", filename);
 		free(shdrs);
 		return (0);
 	}
-	entries = build_entries32(fd, syms, shdrs, symcount,
-				strtab_off, shnum, swap);
+	syms = read_sym_table32(fd, shdrs, sym_idx, str_idx,
+				&symcount, &strtab_off, swap);
+	if (!syms)
+		goto cleanup;
+	entries = malloc(symcount * sizeof(sym_entry_t));
 	if (!entries)
 		goto cleanup;
+	for (i = 0; i < symcount; i++)
+		entries[i] = build_entry32(fd, &syms[i], shdrs,
+					strtab_off, shnum, swap);
 	print_symbols(entries, symcount, 0);
 	free_symbols(entries, symcount);
 	ret = 0;
--- nm_objdump/elf_dispatch.c
+++ nm_objdump/elf_dispatch.c
@@ -1,6 +1,54 @@
 #include "hnm.h"
 
 /**
+ * check_elf_magic - verify ELF magic bytes
+ * @ident: ELF identity bytes
+ *
+ * Return: 1 if valid ELF, 0 otherwise
+ */
+static int check_elf_magic(unsigned char *ident)
+{
+	return (ident[0] == ELFMAG0 && ident[1] == ELFMAG1 &&
+		ident[2] == ELFMAG2 && ident[3] == ELFMAG3);
+}
+
+/**
+ * open_and_read_ident - open file and read ELF identity
+ * @filename: path to file
+ * @ident: buffer to store identity bytes
+ *
+ * Return: file descriptor on success, -1 on error
+ */
+static int open_and_read_ident(const char *filename,
+		unsigned char *ident)
+{
+	int fd;
+	ssize_t n;
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+	{
+		fprintf(stderr, "hnm: %s: No such file or directory\n",
+			filename);
+		return (-1);
+	}
+	n = read(fd, ident, EI_NIDENT);
+	if (n < EI_NIDENT || !check_elf_magic(ident))
+	{
+		fprintf(stderr, "hnm: %s: file format not recognized\n",
+			filename);
+		close(fd);
+		return (-1);
+	}
+	if (lseek(fd, 0, SEEK_SET) < 0)
+	{
+		close(fd);
+		return (-1);
+	}
+	return (fd);
+}
+
+/**
  * process_elf_file - open and dispatch ELF file to correct handler
  * @filename: path to ELF file
  *
@@ -10,46 +58,14 @@ int process_elf_file(const char *filename)
 {
 	int fd;
 	unsigned char ident[EI_NIDENT];
-	ssize_t n;
 
-	fd = open(filename, O_RDONLY);
+	fd = open_and_read_ident(filename, ident);
 	if (fd < 0)
-	{
-		fprintf(stderr, "hnm: %s: No such file or directory\n",
-			filename);
 		return (1);
-	}
-
-	n = read(fd, ident, EI_NIDENT);
-	if (n < EI_NIDENT)
-	{
-		fprintf(stderr, "hnm: %s: file format not recognized\n",
-			filename);
-		close(fd);
-		return (1);
-	}
-
-	if (ident[0] != ELFMAG0 || ident[1] != ELFMAG1 ||
-	    ident[2] != ELFMAG2 || ident[3] != ELFMAG3)
-	{
-		fprintf(stderr,
-			"hnm: %s: file format not recognized\n",
-			filename);
-		close(fd);
-		return (1);
-	}
-
-	if (lseek(fd, 0, SEEK_SET) < 0)
-	{
-		close(fd);
-		return (1);
-	}
-
 	if (ident[EI_CLASS] == ELFCLASS32)
 		return (process_elf32(fd, ident, filename));
-	else if (ident[EI_CLASS] == ELFCLASS64)
+	if (ident[EI_CLASS] == ELFCLASS64)
 		return (process_elf64(fd, ident, filename));
-
 	fprintf(stderr, "hnm: %s: file format not recognized\n", filename);
 	close(fd);
 	return (1);
@@ -67,15 +83,13 @@ int process_elf32(int fd, unsigned char *ident, const char *filename)
 {
 	int ret;
 
-	(void)filename;
 	if (ident[EI_DATA] == ELFDATA2LSB)
 		ret = read_elf32_le(fd, filename);
 	else if (ident[EI_DATA] == ELFDATA2MSB)
 		ret = read_elf32_be(fd, filename);
 	else
 	{
-		fprintf(stderr,
-			"hnm: %s: unknown byte order\n", filename);
+		fprintf(stderr, "hnm: %s: unknown byte order\n", filename);
 		close(fd);
 		return (1);
 	}
@@ -95,15 +109,13 @@ int process_elf64(int fd, unsigned char *ident, const char *filename)
 {
 	int ret;
 
-	(void)filename;
 	if (ident[EI_DATA] == ELFDATA2LSB)
 		ret = read_elf64_le(fd, filename);
 	else if (ident[EI_DATA] == ELFDATA2MSB)
 		ret = read_elf64_be(fd, filename);
 	else
 	{
-		fprintf(stderr,
-			"hnm: %s: unknown byte order\n", filename);
+		fprintf(stderr, "hnm: %s: unknown byte order\n", filename);
 		close(fd);
 		return (1);
 	}
